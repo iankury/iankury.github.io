bfdbf2ef52b0e8e75cbac6bdd6b84b6439a58e1c
Ian Kury <iankwen@gmail.com>
2021-01-01 22:07:17 -0300

    add yt page

---
+++ data.js
@@ -0,0 +1,44 @@
+const thumb_prefix = 'https://img.youtube.com/vi/'
+const thumb_suffix = '/hqdefault.jpg'
+
+const videos = [
+  'zRk-zDOZJms',
+  '1ZwwXG_1wNQ',
+  'gJZOe-KfHco',
+  'Ne5UEkv3yFU',
+  'gn0UB55-pcE',
+  'gWtGy5GgDPU',
+  '9SYWghuzjKE',
+  '_HdVlT3jU1E',
+  'bOIs1d-c-ec',
+  'GMn8NkOa8wY',
+  '1cbAkdxORTM',
+  'h2VyBjBE8S8',
+  '_UCTzBGIxlQ',
+  'H3x3-22Gi3g',
+  '3CT9V7z0BhM',
+  'E-p6l9nioNI',
+  'ymAYWAbWDhk',
+  'yxos8wMJQqM'
+]
+
+const names = [
+  'Twinkle, Twinkle, Little Star',
+  'Ten Little Indians',
+  'She\'ll Be Coming \'Round the Mountain',
+  'Row Your Boat',
+  'Pop Goes the Weasel',
+  'Polly Wolly Doodle',
+  'My Bonnie Lies Over the Ocean',
+  'Mary Had a Little Lamb',
+  'Humpty Dumpty',
+  'Drunken Sailor',
+  'Billy Boy',
+  'Baa, Baa, Black Sheep',
+  'The Eensy Weensy Spider',
+  'Magikarp Song',
+  'Egg Song (Korean)',
+  'Comme Un Homme',
+  'A Barata',
+  'Naturagua'
+]
