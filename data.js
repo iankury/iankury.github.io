ea4018d2411adcc2ee49f8a418a5bc9310f187e4
Ian Kury <iankwen@gmail.com>
2021-01-10 19:13:59 -0300

    add videos

--- data.js
+++ data.js
@@ -2,43 +2,196 @@ const thumb_prefix = 'https://img.youtube.com/vi/'
 const thumb_suffix = '/hqdefault.jpg'
 
 const videos = [
-  'zRk-zDOZJms',
-  '1ZwwXG_1wNQ',
-  'gJZOe-KfHco',
-  'Ne5UEkv3yFU',
-  'gn0UB55-pcE',
-  'gWtGy5GgDPU',
-  '9SYWghuzjKE',
-  '_HdVlT3jU1E',
-  'bOIs1d-c-ec',
-  'GMn8NkOa8wY',
-  '1cbAkdxORTM',
-  'h2VyBjBE8S8',
-  '_UCTzBGIxlQ',
-  'H3x3-22Gi3g',
-  '3CT9V7z0BhM',
-  'E-p6l9nioNI',
-  'ymAYWAbWDhk',
-  'yxos8wMJQqM'
-]
-
-const names = [
-  'Twinkle, Twinkle, Little Star',
-  'Ten Little Indians',
-  'She\'ll Be Coming \'Round the Mountain',
-  'Row Your Boat',
-  'Pop Goes the Weasel',
-  'Polly Wolly Doodle',
-  'My Bonnie Lies Over the Ocean',
-  'Mary Had a Little Lamb',
-  'Humpty Dumpty',
-  'Drunken Sailor',
-  'Billy Boy',
-  'Baa, Baa, Black Sheep',
-  'The Eensy Weensy Spider',
-  'Magikarp Song',
-  'Egg Song (Korean)',
-  'Comme Un Homme',
-  'A Barata',
-  'Naturagua'
+  [
+    'zRk-zDOZJms',
+    'Twinkle, Twinkle, Little Star'
+  ],
+  [
+    '1ZwwXG_1wNQ',
+    'Ten Little Indians'
+  ],
+  [
+    'gJZOe-KfHco',
+    'She\'ll Be Coming \'Round the Mountain'
+  ],
+  [
+    'Ne5UEkv3yFU',
+    'Row Your Boat'
+  ],
+  [
+    'gn0UB55-pcE',
+    'Pop Goes the Weasel'
+  ],
+  [
+    'gWtGy5GgDPU',
+    'Polly Wolly Doodle'
+  ],
+  [
+    '9SYWghuzjKE',
+    'My Bonnie Lies Over the Ocean'
+  ],
+  [
+    '_HdVlT3jU1E',
+    'Mary Had a Little Lamb'
+  ],
+  [
+    'bOIs1d-c-ec',
+    'Humpty Dumpty'
+  ],
+  [
+    'GMn8NkOa8wY',
+    'Drunken Sailor'
+  ],
+  [
+    '1cbAkdxORTM',
+    'Billy Boy'
+  ],
+  [
+    'h2VyBjBE8S8',
+    'Baa, Baa, Black Sheep'
+  ],
+  [
+    '_UCTzBGIxlQ',
+    'The Eensy Weensy Spider'
+  ],
+  [
+    'H3x3-22Gi3g',
+    'Magikarp Song'
+  ],
+  [
+    '3CT9V7z0BhM',
+    'Egg Song (Korean)'
+  ],
+  [
+    'E-p6l9nioNI',
+    'Comme Un Homme'
+  ],
+  [
+    'ymAYWAbWDhk',
+    'A Barata'
+  ],
+  [
+    'yxos8wMJQqM',
+    'Naturágua'
+  ],
+  [
+    'NccYor_TPlU',
+    'A Whole New World (DE)'
+  ],
+  [
+    'dhW1mh7U6-U',
+    'Tír na nÓg'
+  ],
+  [
+    'H9fXoZmMHK8',
+    'Ciranda dos Bichos'
+  ],
+  [
+    'zt93UvnesEc',
+    'Leãozinho'
+  ],
+  [
+    'RpMrpRPZ0Zs',
+    'Quando eu era um peixinho'
+  ],
+  [
+    'kfinwr3A9fg',
+    'Pomar'
+  ],
+  [
+    'r1kmOlsCHP0',
+    'Bebezinho'
+  ],
+  [
+    'bkr1wS8D-6A',
+    'Gotinha'
+  ],
+  [
+    'v1Pv9cOy-2s',
+    'Tchibum'
+  ],
+  [
+    'nDN403JeYiI',
+    'Vira'
+  ],
+  [
+    'ChIRU7CWdBo',
+    'Bicicleta'
+  ],
+  [
+    'CaTXgmHyMSk',
+    'Lavar as Mãos'
+  ],
+  [
+    'x8VNNyobJRo',
+    'Coloridos'
+  ],
+  [
+    'y9H2KxPpSsA',
+    'Ciranda'
+  ],
+  [
+    'jlNoF8GEGWc',
+    'Vai e Vem das Estações'
+  ],
+  [
+    '_vmxj-adiPo',
+    'A Canoa Virou'
+  ],
+  [
+    'x8JirVDqvmo',
+    'Sapo Jururu'
+  ],
+  [
+    'f7Nt4z8bPvI',
+    'Pombinha Branca'
+  ],
+  [
+    'C3JeNr7vCS4',
+    'Oskindô Lê Lê'
+  ],
+  [
+    'gsSY0i_-Ypw',
+    'Caranguejo, O Cravo e a Rosa'
+  ],
+  [
+    'P7Fb3myaz9g',
+    'Alecrim'
+  ],
+  [
+    'cqp4N_Hqxvs',
+    'Parlendas'
+  ],
+  [
+    'a6rT0x4ZSj4',
+    'Peixe Vivo'
+  ],
+  [
+    'MeDBP8OU6q4',
+    'Rato'
+  ],
+  [
+    'AzLUrmhBJkA',
+    'Planta Bambolê'
+  ],
+  [
+    'e7l0ADonP78',
+    'A Tartaruga e o Lobo'
+  ],
+  [
+    'z_agQAZj0yI',
+    'Canção dos Alienígenas'
+  ],
+  [
+    'AyneRYfBP8M',
+    'Passeio do Bebê'
+  ],
+  [
+    'x5Dm5FcvIOw',
+    'Sopa'
+  ],
+  [
+    '2Ec82tLVywQ',
+    'Bicho Folha e Bicho Pau'
+  ]
 ]
